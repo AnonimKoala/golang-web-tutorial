94766ccaf8946e200df4d54b2d475c11ddb4b45a
Robert Kubasiak <robert2006goetel@gmail.com>
2024-03-14 20:25:31 +0100

    parse data to template

--- 012_hands-on/09_hands-on/main.go
+++ 012_hands-on/09_hands-on/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"html/template"
 	"log"
 	"os"
@@ -19,6 +18,7 @@ type Row struct {
 	Open     string
 	High     string
 	Low      string
+	Close    string
 	Volume   string
 	AdjClose string
 }
@@ -42,11 +42,13 @@ func createRows(data [][]string) []Row {
 			} else if j == 3 {
 				row.Low = field
 			} else if j == 4 {
-				row.Volume = field
+				row.Close = field
 			} else if j == 5 {
+				row.Volume = field
+			} else if j == 6 {
 				row.AdjClose = field
 			} else {
-				//print("Unexpected field!")
+				print("Unexpected field!")
 			}
 		}
 
@@ -69,5 +71,10 @@ func main() {
 	}
 
 	rows := createRows(data)
-	fmt.Printf("%+v\n", rows)
+
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", rows)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
